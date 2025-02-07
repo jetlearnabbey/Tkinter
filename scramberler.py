8a2366dd262001ba30960d0a5ff51b57db036d9e
jetlearnabbey <abbey.edvard@gmail.com>
2025-02-07 15:54:08 +0000

    Create scramberler.py

---
+++ scramberler.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+
+
+
+window = Tk()
+window.config(bg="black")
+window.geometry("800x800")
+
+list=[]
+words = ["london","evening","happy","coding","home","egg","table","games","hyper","realistic","computer","cat","sad","omnipotent","arms"]
+testing = random.sample(words[0],len(words[0]))
+print("".join(testing))
+
+for i 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
